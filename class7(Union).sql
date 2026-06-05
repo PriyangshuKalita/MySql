c1121dd47332782176e18e7e4e7e8581561ddcc7
PriyangshuKalita <priyangshukalita1@gmail.com>
2026-06-05 10:45:47 +0530

    SQL codes

---
+++ class7(Union).sql
@@ -0,0 +1,28 @@
+-- Unions
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION distinct
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+SELECT first_name, last_name, 'Old Man' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'male'
+UNION
+SELECT first_name, last_name, 'Old Lady' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'female'
+UNION 
+SELECT first_name, last_name, 'Highly paid employee' AS Label
+FROM employee_salary
+WHERE salary > 70000
+ORDER BY first_name, last_name;

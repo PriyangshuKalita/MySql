ef40a7ea39ee352cded395e7967054d2697bd13d
PriyangshuKalita <priyangshukalita1@gmail.com>
2026-06-05 22:04:29 +0530

    Add temporary tables SQL

---
+++ 14_temporary_tables.sql
@@ -0,0 +1,28 @@
+-- Temporary tables
+
+
+CREATE TEMPORARY TABLE temp_table
+(
+first_name varchar(50),
+last_name varchar(50),
+fav_movie varchar(100)
+);
+
+SELECT *
+FROM temp_table;
+
+
+INSERT INTO temp_table
+VALUES('suraj','ali','naruto');
+
+SELECT *
+FROM temp_table;
+
+
+CREATE TEMPORARY TABLE salary_over_50k
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT *
+FROM salary_over_50k;
